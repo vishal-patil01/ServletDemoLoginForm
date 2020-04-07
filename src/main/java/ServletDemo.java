8b0319d3f6af23ead86c7fb0c1792e60284fe8ac
vishal-patil01 <vishalpatil01@hotmail.com>
2020-04-07 22:25:20 +0530

    ADDED : Servlet To Print Welcome To Servlet

---
+++ src/main/java/ServletDemo.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Welcome To Servlet Application</h2>");
+        out.close();
+    }
+}
+
